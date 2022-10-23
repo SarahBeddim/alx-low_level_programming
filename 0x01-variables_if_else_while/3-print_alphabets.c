382be87d991d7b692ce0d9ca6cda5979ecc10aa2
sarahbeddim <sarahbeddim805@gmail.com>
2022-10-23 04:04:36 -0700

    up to 9

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		putchar(alp);
+	}
+	/* Printing the upper case alphabet*/
+	for (alp = 'A'; alp <= 'Z'; alp++)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		if (alp != e && alp != q)
+		{
+			putchar(alp);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
