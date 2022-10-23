878a42e43aaf221497f4fb8679ad74076e5205b1
SarahBeddim <114090791+SarahBeddim@users.noreply.github.com>
2022-10-23 07:22:19 -0400

    Add files via upload

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point to print _putchar
+ *
+ * Return: will return 0
+ */
+int main(void)
+{
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(word[i]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
